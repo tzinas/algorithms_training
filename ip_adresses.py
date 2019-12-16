63b26cba7530927f9fa972dba67508e245933e4c
tzinas <atzinas2001@gmail.com>
2019-12-16 18:14:10 +0200

    Makes ip_addresses.py code more clear

--- ip_adresses.py
+++ ip_adresses.py
@@ -9,16 +9,16 @@ def make_int(ip):
         ip = ip.split('.')
         return ip
     ip = spl(ip)
-    number_string = ['']
+    number = ['']
     for j in range(3):
         if int(ip[j]) < 100:
-            number_string[0] += '0'
+            number[0] += '0'
         if int(ip[j]) < 10:
-            number_string[0] += '0'
-        number_string[0] += ip[j]
-    number_string[0] = int(number_string[0])
-    number_string.append(int(ip[-1])) 
-    return number_string
+            number[0] += '0'
+        number[0] += ip[j]
+    number[0] = int(number[0])
+    number.append(int(ip[-1])) 
+    return number
 
 useless_routers = []
 
@@ -27,8 +27,12 @@ for start_ip, end_ip in ip_ranges:
         useless_routers.append(end_ip)
 
 for router in useless_routers:
+    router_first_digits = make_int(router)[0] 
     for start_ip, end_ip in ip_ranges:
-        if make_int(router)[0] >= make_int(start_ip)[0] and (make_int(router)[0] < make_int(end_ip)[0] or (make_int(end_ip)[1] != 1 and make_int(router)[0] == make_int(end_ip)[0])):
+        start_ip_first_digits = make_int(start_ip)[0]
+        end_ip_first_digits = make_int(end_ip)[0]
+        end_ip_last_digits = make_int(end_ip)[1]
+        if router_first_digits >= start_ip_first_digits and (router_first_digits < end_ip_first_digits or (end_ip_last_digits != 1 and router_first_digits == end_ip_fist_digits)):
             useless_routers.remove(router)
 
 print(useless_routers)
